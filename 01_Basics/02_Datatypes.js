fcd93a6b6cb296a5e9652fd64bb5f3ac4c525b83
Daksh Rana <115612823+Dax-Rana18@users.noreply.github.com>
2025-03-04 12:26:00 +0530

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+//
+"use strict"; //treat all JS code as newer version: otehrwise automatic only 
+
+//alert("hello") //run on js engine in browser, we are using nodeJs not browser
+
+//*we usually avoid semicolons 
+//*readibility of code 
+// console.log(3+3); console.log("Dax")
+
+let name="Dax"
+let ag=18
+let isLoggedIn=false;
+
+//number => 2 to the power of 53
+//bigInt
+//string=> ""
+//boolean => true / false
+//null => standalone value ,representation of empty value 
+//undefined => not defined value to a declared variable ,like let a;
+//symbol => unique
+
+//object 
+
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
+
